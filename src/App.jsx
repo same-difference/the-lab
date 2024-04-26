3fb8fb4b871875f76f02ca88cee4adf6f84b7150
same-difference <dnmurillo@gmail.com>
2024-04-25 21:39:36 -0400

    added new start screen and responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,21 @@ import Ending from "./components/Ending";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadLinksPreset } from "@tsparticles/preset-links";
 
+const true_start_classies = {
+  state: "true_start",
+  appies: "h-dvh w-screen bg-teal-300 bodyfont flex flex-col justify-center items-center z-0",
+  scene: "w-3/5 h-fit my-10 p-2 text-teal-900 bg-teal-100/90 flex flex-col outline outline-offset-4 outline-2 outline-teal-200 self-center text-2xl rounded-md z-20",
+  story: "text-2xl z-20",
+  choices: "text-xl z-20 text-center text-teal-900 hover:text-teal-700 hover:cursor-pointer hover:tracking-wider w-50 mx-auto py-0.5 mt-4 px-3 outline outline-offset-2 outline-1 hover:outline-2 hover:outline-offset-4 outline-teal-500/50 hover:outline-teal-500/90 rounded-2xl bg-white/80",
+  ending: "hidden"
+}
 
 const start_classies = {
   state: "start",
   appies: "h-dvh w-screen bg-teal-300 bodyfont flex flex-col justify-center items-center z-0",
-  scene: "w-1/5 h-2/3 text-teal-900 bg-teal-100/90 flex flex-col outline outline-offset-4 outline-2 outline-teal-200 self-center text-2xl rounded-md z-20",
+  scene: "w-3/4 md:w-2/5 xl:w-1/5 h-fit text-teal-900 bg-teal-100/90 flex flex-col outline outline-offset-4 outline-2 outline-teal-200 self-center text-2xl rounded-md z-20",
   story: "text-2xl z-20",
-  choices: "text-xl z-20 text-center text-teal-900 hover:text-teal-700 hover:cursor-pointer hover:tracking-wider w-fit mx-auto py-0.5 mt-4 px-3 outline outline-offset-2 outline-1 hover:outline-2 hover:outline-offset-4 outline-teal-300/20 hover:outline-teal-500/90 rounded-2xl bg-white/80",
+  choices: "text-xl mb-10 z-20 text-center text-teal-900 hover:text-teal-700 hover:cursor-pointer hover:tracking-wider w-fit mx-auto py-0.5 mt-4 px-3 outline outline-offset-2 outline-1 hover:outline-2 hover:outline-offset-4 outline-teal-500/50 hover:outline-teal-500/90 rounded-2xl bg-white/80",
   ending: "hidden"
 }
 
@@ -23,7 +31,7 @@ const bulletin_classies = {
   appies: "h-dvh w-screen bg-teal-300 bodyfont flex flex-col items-center z-0",
   scene: "w-11/12 my-7 text-teal-900 flex flex-col text-2xl z-20",
   story: "text-2xl z-20",
-  choices: "text-xl z-20 text-center text-teal-900 hover:text-teal-700 hover:cursor-pointer hover:tracking-wider w-fit mx-auto py-0.5 mt-4 px-3 outline outline-offset-2 outline-1 hover:outline-2 hover:outline-offset-4 outline-teal-300/20 hover:outline-teal-500/90 rounded-2xl bg-white/80",
+  choices: "text-xl z-20 text-center text-teal-900 hover:text-teal-700 hover:cursor-pointer hover:tracking-wider w-fit mx-auto py-0.5 mt-4 px-3 outline outline-offset-2 outline-1 hover:outline-2 hover:outline-offset-4 outline-teal-500/50 hover:outline-teal-500/90 rounded-2xl bg-white/80",
   ending: "hidden"
 }
 
@@ -32,7 +40,7 @@ const choice_classies = {
   appies: "h-dvh w-screen bg-teal-300 bodyfont flex flex-col items-center z-0",
   scene: "w-11/12 my-7 text-teal-900 flex flex-col text-2xl z-20",
   story: "text-2xl z-20",
-  choices: "text-xl z-20 text-center text-teal-900 hover:text-teal-700 hover:cursor-pointer hover:tracking-wider w-fit mx-auto py-0.5 mt-4 px-3 outline outline-offset-2 outline-1 hover:outline-2 hover:outline-offset-4 outline-teal-300/20 hover:outline-teal-500/90 rounded-2xl bg-white/80",
+  choices: "text-xl z-20 text-center text-teal-900 hover:text-teal-700 hover:cursor-pointer hover:tracking-wider w-fit mx-auto py-0.5 mt-4 px-3 outline outline-offset-2 outline-1 hover:outline-2 hover:outline-offset-4 outline-teal-500/50 hover:outline-teal-500/90 rounded-2xl bg-white/80",
   ending: "hidden"
 }
 
@@ -57,8 +65,8 @@ const dispatchToProps = dispatch => ({
 })
 
 function App({tags, currentChoices, sceneText, ending, makeChoice}) {
-  const [ init, setInit ] = useState(false);
-  const [classies, setClassies] = useState(start_classies);
+  const [init, setInit] = useState(false);
+  const [classies, setClassies] = useState(true_start_classies);
 
   useEffect(() => {
     initParticlesEngine(async (engine) => {
@@ -73,7 +81,10 @@ function App({tags, currentChoices, sceneText, ending, makeChoice}) {
   };
   
   useEffect(() => {
-    if (tags.screen == "start") {
+    if (classies.state == "true_start") {
+      setClassies(true_start_classies);
+    }
+    else if (tags.screen == "start") {
       setClassies(start_classies);
     }
     else if (tags.screen == "bulletin") {
@@ -82,7 +93,7 @@ function App({tags, currentChoices, sceneText, ending, makeChoice}) {
     else if (tags.screen == "choice") {
       setClassies(choice_classies);
     }
-  }, [tags.screen]);
+  }, [tags.screen, classies.state]);
 
   useEffect(() => {
     if (ending) {
@@ -90,11 +101,12 @@ function App({tags, currentChoices, sceneText, ending, makeChoice}) {
     }
   }, [ending]);
 
+  
+
   return(
     <div className={"App " + classies.appies}>
       {init && <Particles id="tsparticles z-10" particlesLoaded={particlesLoaded} options={{background: { opacity: 0, }, preset: "links"}} />}
-      
-      <Scene tags={tags} classies={classies.scene} classState={classies.state} 
+      <Scene tags={tags} classies={classies.scene} classState={classies.state} setClassState={setClassies}
       story={<Story storytext={sceneText} classies={classies.story} />}
       choices={<Choices choices={currentChoices} makeChoice={makeChoice} classies={classies.choices} />} />
       
--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,16 +1,27 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Typewriter from 'typewriter-effect';
 
-export default function Scene({ tags, classies, classState, story, choices }) {
+export default function Scene({ tags, classies, classState, setClassState, story, choices }) {
   const [content, setContent] = useState();
 
   useEffect(() => {
-    if (classState == "start") setContent(startContent);
+    if (classState == "true_start") setContent(trueStartContent);
+    else if (classState == "start") setContent(startContent);
     else if (classState == "bulletin") setContent(bulletinContent);
     else if (classState == "choice") setContent(choiceContent);
     else if (classState == "ending") setContent(endingContent);
 
   }, [classState, choices]);
+
+  let trueStartContent=<>
+    <h1 className="text-4xl mt-10 mb-6 text-teal-900 headfont text-center">Welcome to <strong className="text-teal-700">The Lab</strong></h1>
+    <iframe className="w-[448px] max-w-full aspect-video mx-auto" src="https://www.youtube.com/embed/ZVeHn-Oalos?si=gaC_Bo4zj3mOuRw9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <p className="w-3/4 mt-3 mx-auto text-md"><span className="text-teal-700">The Lab</span> is a choose-your-own-adventure game set in a magical laboratory.</p>
+    <p className="w-3/4 mx-auto py-4 text-base">In it, you play as Erin, a young woman with chronic illness. This limits the amount of energy she has in a day, the amount of which she typically cannot predict.</p>
+    <p className="w-3/4 mx-auto text-base">The amount can be described, though. This is done using <a href="https://en.wikipedia.org/wiki/Spoon_theory" target="_blank" className="text-teal-700 underline cursor-pointer">spoons</a> 🥄.</p>
+    <p className="w-3/4 mx-auto pt-10 text-base">You must complete all your tasks for the week, no matter how many spoons you have.</p>
+    <button onClick={() => setClassState("start")} className="text-xl z-20 text-center text-teal-900 hover:text-teal-700 hover:cursor-pointer hover:tracking-wider w-fit mx-auto py-0.5 mt-8 mb-10 px-3 outline outline-offset-2 outline-1 hover:outline-2 hover:outline-offset-4 outline-teal-500/50 hover:outline-teal-500/90 rounded-2xl bg-white/80">Let's Do This</button>
+  </>
 
   let startContent = <>
   <img src="erin.png" className="rounded-full bg-center w-fit aspect-square bg-origin-content bg-auto p-8 self-center" />
